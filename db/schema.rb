cb3700c0654841d38e97ecd8b0f22a596b8010c5
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-08-11 23:32:04 -0500

    status model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_042056) do
+ActiveRecord::Schema.define(version: 2022_08_12_042531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2022_08_12_042056) do
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "thread_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
